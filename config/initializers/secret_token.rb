0cd36d7c7629472d021e2ad180d2f93e88322bbb
teacplusplus <teacplusplus@gmail.com>
2014-03-05 03:14:49 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaborWebPing::Application.config.secret_key_base = 'a70c9719e7a68651de279806fd6e54050df265ed47f53c1b79a6d162ad9365f31d769a6ebf48b50f66bda5822649c6e0485f65c339ed0f6f09a3e865a3ae5fe9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaborWebPing::Application.config.session_store :cookie_store, key: '_tabor_web_ping_session'
